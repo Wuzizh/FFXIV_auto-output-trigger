36b769e5dfc1a16ffb5d841d6941577116511937
yoyokity <1125404308@qq.com>
2020-03-12 03:03:10 +0800

    Update (请管理员运行)添加鲶鱼精邮差端口到环境变量.bat

--- souma版GCD合并整合/(请管理员运行)添加鲶鱼精邮差端口到环境变量.bat
+++ souma版GCD合并整合/(请管理员运行)添加鲶鱼精邮差端口到环境变量.bat
@@ -1,4 +1,4 @@
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 @echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PostNamazu /t REG_SZ /d "2019" /f
 pause
